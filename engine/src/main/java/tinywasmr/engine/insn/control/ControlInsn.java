22730b160964bbe4a3887d020dc861dc447aec0f
nahkd123 <nahkd123@gmail.com>
2024-11-09 01:12:26 +0700

    Redo return insn to respect the function return type

--- engine/src/main/java/tinywasmr/engine/insn/control/ControlInsn.java
+++ engine/src/main/java/tinywasmr/engine/insn/control/ControlInsn.java
@@ -2,36 +2,52 @@ package tinywasmr.engine.insn.control;
 
 import java.util.List;
 
+import tinywasmr.engine.exec.ValidationException;
+import tinywasmr.engine.exec.frame.Frame;
 import tinywasmr.engine.exec.frame.FunctionFrame;
-import tinywasmr.engine.exec.trap.ExternalTrap;
 import tinywasmr.engine.exec.trap.ModuleTrap;
 import tinywasmr.engine.exec.value.Value;
 import tinywasmr.engine.exec.vm.Machine;
 import tinywasmr.engine.insn.Instruction;
+import tinywasmr.engine.type.value.ValueType;
 
 public enum ControlInsn implements Instruction {
-	NOP,
-	UNREACHABLE,
-	RETURN;
-
-	@Override
-	public void execute(Machine vm) {
-		switch (this) {
-		case NOP:
-			return;
-		case UNREACHABLE:
+	NOP {
+		@Override
+		public void execute(Machine vm) {}
+	},
+	UNREACHABLE {
+		@Override
+		public void execute(Machine vm) {
 			vm.setTrap(new ModuleTrap());
-			return;
-		case RETURN: {
-			List<Value> results = vm.popFrame().getOperandStack();
-			while (!(vm.peekFrame() instanceof FunctionFrame)) vm.popFrame();
-			vm.popFrame();
+		}
+	},
+	RETURN {
+		@Override
+		public void execute(Machine vm) {
+			FunctionFrame func = vm.peekFunctionFrame();
+			List<ValueType> resultTypes = vm.peekFunctionFrame().getBranchResultTypes().blockResults();
+			Value[] results = new Value[resultTypes.size()];
+
+			for (int i = results.length - 1; i >= 0; i--) {
+				Value val = vm.peekFrame().popOprand();
+				if (vm.hasRuntimeValidation() && !val.type().equals(resultTypes.get(i)))
+					throw new ValidationException("Type mismatch: %s (stack) != %s (block)"
+						.formatted(val.type(), resultTypes.get(i)));
+				results[i] = val;
+			}
+
+			Frame poppedFrame;
+			while ((poppedFrame = vm.popFrame()) != func) {
+				if (poppedFrame instanceof FunctionFrame && poppedFrame != func)
+					throw new RuntimeException("Popped wrong function frame, check VM implementation");
+			}
+
 			for (Value val : results) vm.peekFrame().pushOperand(val);
 			return;
 		}
-		default:
-			vm.setTrap(new ExternalTrap(new RuntimeException("Unable to execute %s".formatted(this))));
-			return;
-		}
-	}
+	};
+
+	@Override
+	public abstract void execute(Machine vm);
 }
